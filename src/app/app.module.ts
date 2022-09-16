53f2bfb2b39cfbbf99533aadb9136d8af9eee112
User <girain3181@gmail.com>
2022-09-16 04:41:37 +0300

    start

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,31 @@ import { AppComponent } from './app.component';
 import {HttpClientModule} from "@angular/common/http";
 import { UsersComponent } from './componets/users/users.component';
 import { UserComponent } from './componets/user/user.component';
+import {Route, RouterModule, Routes} from "@angular/router";
+import { PostsComponent } from './componets/posts/posts.component';
+import { PostComponent } from './componets/post/post.component';
+import { UserDetalisComponent } from './componets/user-detalis/user-detalis.component';
+import { PostsDetalisComponent } from './componets/posts-detalis/posts-detalis.component';
+
+const routes:Routes = [
+  {path:"users", component:UsersComponent , children:[{path:'detalis/:id',component:UserDetalisComponent}]},
+  {path:"posts", component:PostsComponent, children:[{path: 'detalis/:id',component: PostsDetalisComponent}]}
+]
 
 @NgModule({
   declarations: [
     AppComponent,
     UsersComponent,
-    UserComponent
+    UserComponent,
+    PostsComponent,
+    PostComponent,
+    UserDetalisComponent,
+    PostsDetalisComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/modules/IUserDetalis.ts
@@ -0,0 +1,8 @@
+import {IUser} from "./IUser";
+
+export interface IUserDetalis extends IUser{
+
+  username:string,
+  phone:string,
+  website:string
+}
